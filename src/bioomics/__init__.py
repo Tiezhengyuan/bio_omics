a051d807fb6cc8b3a073c96276db73ac47d768db
Tiezheng Yuan <tiezhengyuan@hotmail.com>
2024-01-26 21:00:49 -0500

    add constant ANATOMY_GROUPS

--- src/bioomics/__init__.py
+++ src/bioomics/__init__.py
@@ -4,7 +4,8 @@ from .connector.conn_ftplib import ConnFTPlib
 from .connector.conn_redis import ConnRedis
 
 # database
-from .ncbi import NCBI
+from .ncbi import NCBI, ANATOMY_GROUPS
 from .rnacentral import RNACentral
 from .mirbase import Mirbase
 from .expasy import Expasy
+
