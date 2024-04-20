dbcf89505628a103c1bdc60a6b815846df87817f
Tiezheng Yuan <tiezhengyuan@hotmail.com>
2024-04-20 12:13:49 -0400

    entity: epitope from IEDB

--- src/bioomics/__init__.py
+++ src/bioomics/__init__.py
@@ -1,14 +1,22 @@
+# 
+from .integrate_data import IntegrateData
+
 # connector
 from .connector.conn_http import ConnHTTP
 from .connector.conn_ftp import ConnFTP
 from .connector.conn_ftplib import ConnFTPlib
 from .connector.conn_redis import ConnRedis
 
-# database
+# comprehensive database
 from .ncbi import NCBI, ANATOMY_GROUPS
+from .expasy import Expasy
+
+# RNA, non-coding RNA
 from .rnacentral import RNACentral
 from .mirbase import Mirbase
-from .expasy import Expasy
+
+# immuno-biology
 from .iedb import IEDB
 from .iedb_antigen import IEDBAntigen
-from .integrate_data import IntegrateData
+from .iedb_epitope import IEDBEpitope
+
