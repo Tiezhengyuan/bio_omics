7edd324310bd7b2bbeaf99fa3f7969e3a556b550
Tiezheng Yuan <tiezhengyuan@hotmail.com>
2024-04-21 20:54:13 -0400

    get data from json

--- src/bioomics/iedb.py
+++ src/bioomics/iedb.py
@@ -82,63 +82,324 @@ class IEDB:
             return local_file
         return None
 
-    def antigen(self, local_csv:str):
+    def download_json(self, type:str):
         '''
-        unique ID: accession in Antigen IRI
+        type: epitope|antigen|tcell|bcell|reference etc
         '''
-        df = pd.read_csv(local_csv, compression='zip', header=1, sep=',')
+        _names = {
+            'antigen': f"antigen_full_{self.version}_json.zip",
+            'epitope': f"epitope_full_{self.version}_json.zip",
+            'tcell': f"tcell_full_{self.version}_json.zip",
+            'bcell': f"bcell_full_{self.version}_json.zip",
+            'mhc': "mhc_ligand_full_json.zip",
+            'reference': f"reference_full_{self.version}_json.zip",
+            'tcr': f"tcr_full_{self.version}_json.zip",
+            'bcr': f"bcr_full_{self.version}_json.zip",
+        }
+        if type in _names:
+            conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+            file_name = _names[type] 
+            end_point = f"/downloader.php?file_name=doc/{file_name}"
+            _, local_file = conn.download_file(end_point, file_name)
+            return local_file
+        return None
+
+    def read_json(self, local_file:str) -> list:
+        # read json
+        data_list = []
+        from zipfile import ZipFile
+        with ZipFile(local_file) as zf:
+            for file in zf.namelist():
+                with zf.open(file) as f:
+                    data = json.load(f)
+                    data_list += data['Data']
+        return data_list
+    
+    def antigen_csv(self) -> dict:
+        # download
+        file_name = f"antigen_full_{self.version}.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        df = pd.read_csv(local_file, compression='zip', header=1, sep=',')
         data_list = df.replace({np.nan:None}).to_dict(orient='records')
-        entity_data, n = {}, 1
-        for data in data_list:
-            key = None
-            if data["Antigen IRI"]:
-                data['accession'] = os.path.basename(data["Antigen IRI"])
-                key = data['accession']
-            if data["Organism IRI"] and "Taxon" in data["Organism IRI"]:
-                taxon_id = os.path.basename(data["Organism IRI"])
-                data['taxon_id'] = taxon_id.split('_')[-1]
-            if key is None:
-                print(data)
-            entity_data[key] = data
-            n += 1
-            # if n == 100:
-            #     break
+        entity_data = self.convert_data(data_list, 'accession')
         return entity_data
 
-    def epitope(self, local_csv:str):
+    def antigen_json(self) -> dict:
+        '''
+        unique ID: accession number
+        '''
+        # download
+        file_name = f"antigen_full_{self.version}_json.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+       
+        # retrieve data
+        data_list = self.read_json(local_file)
+        entity_data = self.convert_data(data_list, 'accession')
+        return entity_data
+
+
+    def epitope_csv(self) -> dict:
         '''
         unique ID: epitope_id in "IEDB IRI"
         '''
-        df = pd.read_csv(local_csv, compression='zip', header=1, low_memory=False)
+        # download
+        file_name = f"epitope_full_{self.version}.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        df = pd.read_csv(local_file, compression='zip', header=1, low_memory=False)
         data_list = df.replace({np.nan:None}).to_dict(orient='records')
-        entity_data, n = {}, 1
-        for data in data_list:
-            if data["Molecule Parent IRI"]:
-                data['accession'] = os.path.basename(data["Molecule Parent IRI"])
-            # detect unique key
-            key = None
-            if data["IEDB IRI"]:
-                data['epitope_id'] = os.path.basename(data["IEDB IRI"])
-                key = data['epitope_id']
-            if key is None:
-                print(data)
-            entity_data[key] = data
-            n += 1
-            # if n == 1000:
-            #     break
+        entity_data = self.convert_data(data_list, 'epitope_id')
+        return entity_data
+
+    def epitope_json(self) -> dict:
+        '''
+        '''
+        # download
+        file_name = f"epitope_full_{self.version}_json.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # retrieve data
+        data_list = self.read_json(local_file)
+        entity_data = self.convert_data(data_list, 'epitope_id')
+        return entity_data
+        
+    def tcr_csv(self):
+        '''
+        T-cell receptor
+        unique ID: receptor_id
+        '''
+        # download
+        file_name = f"tcr_full_{self.version}.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        df = pd.read_csv(local_file, compression='zip', header=1, low_memory=False)
+        data_list = df.replace({np.nan:None}).to_dict(orient='records')
+        entity_data = self.convert_data(data_list, 'receptor_id')
+        return entity_data
+
+    def tcr_json(self):
+        '''
+        T-cell receptor
+        unique ID: receptor_id
+        '''
+        # download
+        file_name = f"tcr_full_{self.version}_json.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        data_list = self.read_json(local_file)
+        entity_data = self.convert_data(data_list, 'receptor_id')
+        return entity_data
+
+    def tcell_csv(self):
+        '''
+        T-cell
+        unique ID: assay_id
+        '''
+        # download
+        file_name = f"tcell_full_{self.version}.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        df = pd.read_csv(local_file, compression='zip', header=1, low_memory=False)
+        data_list = df.replace({np.nan:None}).to_dict(orient='records')
+        entity_data = self.convert_data(data_list, 'assay_id')
+        return entity_data
+
+    def tcell_json(self):
+        '''
+        T-cell
+        unique ID: assay_id
+        '''
+        # download
+        file_name = f"tcell_full_{self.version}_json.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        data_list = self.read_json(local_file)
+        entity_data = self.convert_data(data_list, 'assay_id')
         return entity_data
     
-
-    def reference(self, local_csv:str):
+    def bcr_csv(self):
         '''
-        unique ID: reference_id in IEDB IRI
+        B-cell receptor
+        unique ID: receptor_id
         '''
-        df = pd.read_csv(local_csv, compression='zip', header=1, sep=',')
-        entity_data, n = {}, 1
+        # download
+        file_name = f"bcr_full_{self.version}.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        df = pd.read_csv(local_file, compression='zip', header=1, low_memory=False)
         data_list = df.replace({np.nan:None}).to_dict(orient='records')
-        for data in data_list:
-            if data["IEDB IRI"]:
-                data['reference_id'] = str(os.path.basename(data["IEDB IRI"]))
-                key = data['reference_id']
-                entity_data[key] = data
+        entity_data = self.convert_data(data_list, 'receptor_id')
         return entity_data
+
+    def bcr_json(self):
+        '''
+        B-cell receptor
+        unique ID: receptor_id
+        '''
+        # download
+        file_name = f"bcr_full_{self.version}_json.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        data_list = self.read_json(local_file)
+        entity_data = self.convert_data(data_list, 'receptor_id')
+        return entity_data
+
+    def bcell_csv(self):
+        '''
+        B-cell
+        unique ID: assay_id
+        '''
+        # download
+        file_name = f"bcell_full_{self.version}_single_file.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        df = pd.read_csv(local_file, compression='zip', header=1, low_memory=False)
+        data_list = df.replace({np.nan:None}).to_dict(orient='records')
+        entity_data = self.convert_data(data_list, 'assay_id')
+        return entity_data
+
+    def bcell_json(self):
+        '''
+        B-cell
+        unique ID: assay_id
+        '''
+        # download
+        file_name = f"bcell_full_{self.version}_json.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        data_list = self.read_json(local_file)
+        entity_data = self.convert_data(data_list, 'assay_id')
+        return entity_data
+
+    def mhc_csv(self):
+        '''
+        major histocompatibility complex
+        unique ID: assay_id
+        '''
+        # download
+        file_name = f"mhc_ligand_full_single_file.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        df = pd.read_csv(local_file, compression='zip', header=1, low_memory=False)
+        data_list = df.replace({np.nan:None}).to_dict(orient='records')
+        entity_data = self.convert_data(data_list, 'assay_id')
+        return entity_data
+
+    def mhc_json(self):
+        '''
+        major histocompatibility complex
+        unique ID: assay_id
+        '''
+        # download
+        file_name = f"mhc_ligand_full_json.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        data_list = self.read_json(local_file)
+        entity_data = self.convert_data(data_list, 'assay_id')
+        return entity_data
+    
+    def reference_csv(self):
+        '''
+        referene
+        unique ID: reference_id
+        '''
+        # download
+        file_name = f"reference_full_{self.version}.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        df = pd.read_csv(local_file, compression='zip', header=1, low_memory=False)
+        data_list = df.replace({np.nan:None}).to_dict(orient='records')
+        entity_data = self.convert_data(data_list, 'reference_id')
+        return entity_data
+
+    def reference_json(self):
+        '''
+        reference
+        unique ID: reference_id
+        '''
+        # download
+        file_name = f"reference_full_{self.version}_json.zip"
+        conn = ConnHTTP(self.url, self.local_path, self.overwrite)
+        end_point = f"/downloader.php?file_name=doc/{file_name}"
+        _, local_file = conn.download_file(end_point, file_name)
+        # convert
+        data_list = self.read_json(local_file)
+        entity_data = self.convert_data(data_list, 'reference_id')
+        return entity_data
+            
+    def convert_data(self, data_list:list, unique_key:str) -> dict:
+        '''
+        convert list to dict
+        detect duplicates and invalid identifier
+        '''
+        entity_data = {}
+        for data in data_list:
+            ids = self.retrieve_ids(data)
+            data.update(ids)
+            # detect unique key
+            if unique_key in data:
+                key = ids[unique_key]
+                entity_data[key] = data
+            else:
+                print(data)
+        return entity_data
+
+    def retrieve_ids(self, data:dict) -> dict:
+        '''
+        retrieve IDs
+        '''
+        ids = {}
+        for key, value in data.items():
+            if value:
+                value = str(value)
+                base_name = os.path.basename(value)
+                if "Antigen IRI" in key or 'Molecule Parent IRI' in key:
+                    ids['accession'] = base_name
+                elif "IEDB IRI" in key:
+                    if "reference" in value:
+                        ids['reference_id'] = base_name
+                    elif "epitope" in value:
+                        ids['epitope_id'] = base_name
+                    elif "assay" in value:
+                        ids['assay_id'] = base_name
+                    
+                elif "Organism" in key and "NCBITaxon" in value:
+                    ids['taxon_id'] = base_name.split('_')[-1]
+                elif key == 'Group IRI' and 'receptor' in value:
+                    ids['receptor_group_id'] = base_name
+                elif 'IEDB Receptor ID' in key:
+                    ids['receptor_id'] = value
+        return ids
+
+
+
--- src/bioomics/iedb_antigen.py
+++ src/bioomics/iedb_antigen.py
@@ -1,5 +1,7 @@
 '''
 build data mapping of antigen from IEDB
+
+antigen --- eptope --- BCR/TCR/BCell/TCell/MHC
 '''
 from biosequtils import Dir
 import os
@@ -8,13 +10,12 @@ from .iedb import IEDB
 from .integrate_data import IntegrateData
 
 class IEDBAntigen(IEDB):
-    entity = 'antigen'
     key = 'accession'
 
     def __init__(self, local_path:str):
         super().__init__(local_path, None, False)
-        self.meta['entity'] = self.entity
-        self.meta['entity_path'] = os.path.join(self.meta['local_path'], self.entity)
+        self.meta['entity'] = 'antigen'
+        self.meta['entity_path'] = os.path.join(self.meta['local_path'], 'antigen')
         Dir(self.meta['entity_path']).init_dir()
         self.integrate = None
     
@@ -22,39 +23,41 @@ class IEDBAntigen(IEDB):
         self.integrate = IntegrateData(self.meta['entity_path'])
         
         # antigen
-        # self.integrate_antigen()
-        #epitope
+        self.integrate_antigen()
+        # epitope
         self.integrate_epitope()
+        # MHC
+        entity_data = self.mhc_json()
+        self.integrate_epitope_related(entity_data, 'MHC')
+        # B-cell
+        entity_data = self.bcell_json()
+        self.integrate_epitope_related(entity_data, 'b_cell')
 
         # 
         self.integrate.save_index_meta()
         self.save_meta(self.meta['entity_path'])
         return True
 
-
-    def download(self, entity:str, file_type:str):
-        if file_type == 'csv':
-            local_file = self.download_csv(entity)
-            key = f"{entity}_csv"
-            self.meta[key] = local_file
-
     def integrate_antigen(self):
-        self.download(self.entity, 'csv')
-        entity_data = self.antigen(self.meta['antigen_csv'])
+        entity_data = self.antigen_json()
         # check if data exists in json
         for json_data in self.integrate.scan():
-            acc = json_data.get(self.key)
+            acc = json_data.get('key')
             if  acc in entity_data:
-                json_data[self.source]=entity_data[acc]
+                if 'antigen' not in json_data:
+                    json_data['antigen'] = {}
+                json_data['antigen'][self.source]=entity_data[acc]
                 self.integrate.save_data(json_data)
                 del entity_data[acc]
         # add new
         for data in entity_data.values():
-            self.integrate.add_data({self.source: data}, data.get(self.key))
+            input = {
+                'antigen': {self.source: data},
+            }
+            self.integrate.add_data(input, data.get(self.key))
 
     def integrate_epitope(self):
-        self.download('epitope', 'csv')
-        entity_data = self.epitope(self.meta['epitope_csv'])
+        entity_data = self.epitope_json()
         # aggregate epitopes by protein
         agg, m = {},0
         for epitope_id, data in entity_data.items():
@@ -69,28 +72,41 @@ class IEDBAntigen(IEDB):
         for json_data in self.integrate.scan():
             acc = json_data['key']
             if acc in agg:
-                json_data[self.source]['epitopes'] = agg[acc]
+                if 'epitopes' not in json_data:
+                    json_data['epitopes'] = {}
+                json_data['epitopes'][self.source] = agg[acc]
                 self.integrate.save_data(json_data)
                 n += len(agg[acc])
-            else:
-                print(json.dumps(json_data, indent=4))
         print(f"{m}-{n}")
 
+
+    def integrate_epitope_related(self, entity_data:dict, inner_key:str):
+        # aggregate related data by epitope_id
+        agg = {}
+        for assay_id, data in entity_data.items():
+            if 'epitope_id' in data:
+                epitope_id = data['epitope_id']
+                if epitope_id not in agg:
+                    agg[epitope_id] = {}
+                agg[epitope_id][assay_id] = data
+        # inject related data into inner_key within that epitope
+        for json_data in self.integrate.scan():
+            tag = 0
+            if 'epitopes' in json_data and self.source in json_data['epitopes']:
+                json_epitopes = json_data['epitopes'][self.source]
+                for epitope_id, epitope_data in json_epitopes.items():
+                    if epitope_id in agg:
+                        epitope_data[inner_key] = agg[epitope_id]
+                        tag = 1
+            if tag == 1:
+                self.integrate.save_data(json_data)
+
+    
+
     def _test(self):
-        import zipfile
         with open('/home/yuan/Downloads/epitope_full_v3.csv', 'r') as f:
             for line in f:
                 if "A3X8Q8" in line:
                     print(line)
 
 
-    # def integrate_reference(self):
-    #     self.download('reference', 'csv')
-    #     entity_data = self.reference(self.meta['reference_csv'])
-    #     for data in self.integrate.scan():
-    #         acc = str(data["# References"])
-    #         if acc and acc in entity_data:
-    #             data["# References"] = entity_data[acc]
-    #             self.integrate.save_data(data)
-    #         else:
-    #             print(data)
